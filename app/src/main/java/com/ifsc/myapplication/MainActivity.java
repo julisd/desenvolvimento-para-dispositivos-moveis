dcc4a531ab4caf2bc5de1bbfbf80009d15bb7807
Juliana <julisd3@gmail.com>
2023-03-02 20:26:20 -0300

    add metade imc

--- app/src/main/java/com/ifsc/myapplication/MainActivity.java
+++ app/src/main/java/com/ifsc/myapplication/MainActivity.java
@@ -7,22 +7,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int contador;
-    TextView textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contador=0;
-        textView=findViewById(R.id.textView);
-        textView.setText(Integer.toString(contador));
-    }
-
-    public void contaClick(View view){
-        contador++;
-        textView.setText(Integer.toString(contador));
-
-
     }
 }
