fc73cd88ccaaca6a569afa2ac5154c02c1ec05a1
Juliana <julisd3@gmail.com>
2023-03-09 21:36:21 -0300

    refatoracao codigo

--- app/src/main/java/com/ifsc/myapplication/MainActivity.java
+++ app/src/main/java/com/ifsc/myapplication/MainActivity.java
@@ -1,43 +1,46 @@
 package com.ifsc.myapplication;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTextPeso, editTextAltura;
-    TextView textViewResultado;
-    ImageView imageView;
+    Fragment fragmentA, fragmentB;
+    Button buttonA, buttonB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextPeso=findViewById(R.id.edPeso);
-        editTextAltura=findViewById(R.id.edAltura);
-        imageView=findViewById(R.id.imageView);
-        textViewResultado=findViewById(R.id.tvResultado);
+        buttonA=findViewById(R.id.buttonA);
+        buttonB=findViewById(R.id.buttonB);
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
     }
 
-    public void calcularIMC(View v){
-        double peso, altura, imc;
-
-        peso=Double.parseDouble(editTextPeso.getText().toString());
-        altura=Double.parseDouble(editTextAltura.getText().toString());
-
-        imc = peso/(altura*altura);
-
-        textViewResultado.setText(Double.toString(imc));
-
-        if(imc >25){
-            imageView.setImageResource(R.drawable.obesidade1);
-        }
-
-
+    public void abreFragmentoA(View view){
+        fragmentA = new BlankFragment();
+        FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentA, fragmentA);
+        transaction.commit();
     }
+
+    public void abreFragmentoB(View view){
+        fragmentB = new BlankFragment2();
+        FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentA, fragmentB);
+        transaction.commit();
+    }
+
 }
