a551e62885159787407979fa80071b6dcb72e638
juliana <julisd3@gmail.com>
2023-04-20 18:05:22 -0300

    add listview

--- app/src/main/java/com/ifsc/myapplication/MainActivity.java
+++ app/src/main/java/com/ifsc/myapplication/MainActivity.java
@@ -1,54 +1,45 @@
 package com.ifsc.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.text.DecimalFormat;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    FrutaController frutaController;
 
-    EditText editTextPeso, editTextAltura;
-    TextView textViewResultado;
-    ImageView imageView;
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextPeso=findViewById(R.id.edPeso);
-        editTextAltura=findViewById(R.id.edAltura);
-        imageView=findViewById(R.id.imageView);
-        textViewResultado=findViewById(R.id.tvResultado);
-        editTextPeso.setText(getIntent().getExtras().getString("editTextPeso"));
+        listView = findViewById(R.id.listView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView tvNome = view.findViewById(R.id.tvNome);
+                String nome = tvNome.getText().toString();
+
+                Toast.makeText(getApplicationContext(), nome, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        frutaController = new FrutaController();
+        atualizaLista();
     }
 
-    public void calcularIMC(View v){
-        double peso, altura, imc;
-
-        peso=Double.parseDouble(editTextPeso.getText().toString());
-        altura=Double.parseDouble(editTextAltura.getText().toString());
-
-        imc = peso/(altura*altura);
-
-        textViewResultado.setText(Double.toString(imc));
-
-
-        if(imc >= 16 && imc <= 18.49 )
-            imageView.setImageResource(R.drawable.abaixopeso);
-        if (imc >= 18.5 && imc <= 24.9)
-            imageView.setImageResource(R.drawable.normal);
-        else if (imc >= 25 && imc <= 29.9)
-            imageView.setImageResource(R.drawable.sobrepeso);
-        else if (imc >= 30 && imc <= 34.9)
-            imageView.setImageResource(R.drawable.obesidade1);
-        else if (imc >= 35 && imc <= 39.9)
-            imageView.setImageResource(R.drawable.obesidade2);
-        else if (imc >= 40)
-            imageView.setImageResource(R.drawable.obesidade3);
-
+    public void atualizaLista(){
+        listView.setAdapter(
+                new ArrayFrutaAdapter(
+                        getApplicationContext(),
+                        R.layout.item_lista,
+                        frutaController.FRUTAS
+                )
+        );
     }
 }
