3afb575636e441056b457f9efbb7a8078d0f9181
Aluno <julisd3@gmail.com>
2023-05-05 00:10:55 -0300

    feito em aula

--- app/src/main/java/com/ifsc/myapplication/MainActivity.java
+++ app/src/main/java/com/ifsc/myapplication/MainActivity.java
@@ -4,61 +4,118 @@ import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    SQLiteDatabase sqLiteDatabase;
+    MediaPlayer mediaPlayer;
+    ImageButton bntPlay,stop, pausa;
+    SeekBar seekBarTime;
+    TextView tvTime;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.seminovos);
 
-        sqLiteDatabase = openOrCreateDatabase("testBd",MODE_PRIVATE, null);
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS nota(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "titulo VARCHAR," +
-                "txt VARCHAR)");
-        insertNota();
+        bntPlay = findViewById(R.id.bntPlay);
+        stop = findViewById(R.id.stop);
+        pausa = findViewById(R.id.pausa);
+        bntPlay.setOnClickListener(this);
+        pausa.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+        seekBarTime = findViewById(R.id.seekBarTime);
+        tvTime = findViewById(R.id.tvTime);
+        timerCounter();
+
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBar.setMax(mediaPlayer.getDuration());
+                mediaPlayer.seekTo(seekBarTime.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
-    public void insertNota(){
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("titulo", "titulo1");
-        contentValues.put("txt", "txt1");
-
-        sqLiteDatabase.insert("nota", null, contentValues);
-    }
-
-    public void atualizaNotas(){
-
-    }
-   @SuppressLint("Range")
-    public ArrayList<String> listNotas(){
-        ArrayList<String> resultado = new ArrayList<>();
-       Cursor c = sqLiteDatabase.rawQuery("Select * from nota",null);
-       c.moveToFirst();
-       while (!c.isAfterLast()){
-           resultado.add(c.getString(c.getColumnIndex("titulo")));
-           c.moveToNext();
-       }
-       return resultado;
-    }
 
     @Override
-    protected void onStart(){
-        super.onStart();
-        ListView listView = findViewById(R.id.listview);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                listNotas()
-        );
-        listView.setAdapter(adapter);
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bntPlay:
+                mediaPlayer.start();
+                timerCounter();
+                break;
+            case R.id.pausa:
+                mediaPlayer.pause();
+                break;
+            case R.id.stop:
+                mediaPlayer.stop();
+                break;
+        }
     }
+
+    public String convertDurationMillis(Integer getDurationInMillis){
+
+        int getDurationMillis = getDurationInMillis;
+        String convertHours = String.format("%02d", TimeUnit.MILLISECONDS.toHours(getDurationMillis));
+        String convertMinutes = String.format("%02d", TimeUnit.MILLISECONDS.toMinutes(getDurationMillis));
+        String convertSeconds = String.format("%02d", TimeUnit.MILLISECONDS.toSeconds(getDurationMillis) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(getDurationMillis)));
+
+        String getDuration = convertHours + ":" + convertMinutes + ":" + convertSeconds;
+
+        return getDuration;
+    }
+
+    public  void atualizaTime() {
+        tvTime.setText(convertDurationMillis(mediaPlayer.getCurrentPosition()) +
+                "/" + convertDurationMillis(mediaPlayer.getDuration()));
+        mediaPlayer.getCurrentPosition();
+
+        seekBarTime.setMax(mediaPlayer.getDuration());
+    }
+    private Timer timer;
+    private void timerCounter(){
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        atualizaTime();
+                        seekBarTime.setProgress(mediaPlayer.getCurrentPosition());
+                    }
+                });
+            }
+        }, 0, 1000);
+    }
+
 }
